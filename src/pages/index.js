396748ee064fa4e3519efb0e5fba3c3e2ad1da42
Dmitriy Ostapenko <dmitriy.ostapenko@outlook.com>
2020-05-09 00:56:21 +0300

    Added container components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import { Container } from "../components"
 
-export default () => <div>Привет семье</div>
+//export default () => <div>Привет семье</div>
+
+const Indexpage = () => {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  )
+}
+
+export default Indexpage
