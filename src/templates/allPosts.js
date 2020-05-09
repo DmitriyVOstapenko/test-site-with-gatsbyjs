e2942f89fa1a561533c28bbdb7b08857ef2c0755
Dmitriy Ostapenko <dmitriy.ostapenko@outlook.com>
2020-05-09 18:34:21 +0300

    Add automatically generate allPosts page

---
+++ src/templates/allPosts.js
@@ -0,0 +1,78 @@
+import React from "react"
+import { graphql } from "gatsby"
+import {
+  Container,
+  Content,
+  ContentCard,
+  FeatureImage,
+  Pagination,
+} from "../components"
+import { H1, P } from "../elements"
+
+const allPosts = ({ pageContext, data }) => {
+  const { currentPage, numPages } = pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? "/" : `/${currentPage - 1}`
+  const nextPage = `/${currentPage + 1}`
+
+  const posts = data.allMdx.edges
+
+  return (
+    <Container>
+      <FeatureImage />
+      <Content>
+        <H1 textAlign="center" margin="0 0 1rem 0">
+          Elit cronus sume text etc.
+        </H1>
+        <P color="dark2" text-align="center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </P>
+        {posts.map(post => (
+          <ContentCard
+            key={post.node.frontmatter.slug}
+            date={post.node.frontmatter.date}
+            title={post.node.frontmatter.title}
+            excerpt={post.node.frontmatter.excerpt}
+            slug={post.node.frontmatter.slug}
+          />
+        ))}
+      </Content>
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
+    </Container>
+  )
+}
+
+export default allPosts
+
+export const pageQuery = graphql`
+  query AllPostsQuery($skip: Int!, $limit: Int!) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      skip: $skip
+      limit: $limit
+    ) {
+      edges {
+        node {
+          frontmatter {
+            slug
+            title
+            date(formatString: "DD MMMM YYYY")
+            excerpt
+          }
+        }
+      }
+    }
+  }
+`
