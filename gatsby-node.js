1563afbbe9043712a258ee33564a8db2cc7ec70d
Dmitriy Ostapenko <dmitriy.ostapenko@outlook.com>
2020-05-09 19:18:41 +0300

    add single posts automatically generation

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,14 +33,14 @@ exports.createPages = async function ({ actions, graphql }) {
     })
   })
 
-  //   // Create single blog posts
-  //   data.allMdx.edges.forEach(edge => {
-  //     const slug = edge.node.frontmatter.slug
-  //     const id = edge.node.id
-  //     actions.createPages({
-  //       path: slug,
-  //       component: require.resolve("./src/templates/singlePost.js"),
-  //       context: { id },
-  //     })
-  //   })
+    // Create single blog posts
+    data.allMdx.edges.forEach(edge => {
+      const slug = edge.node.frontmatter.slug
+      const id = edge.node.id
+      actions.createPage({
+        path: slug,
+        component: require.resolve("./src/templates/singlePost.js"),
+        context: { id },
+      })
+    })
 }
---
+++ src/templates/singlePost.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { H1 } from "../elements"
+import { Container, Post, FeatureImage } from "../components"
+
+const singlePost = ({ data }) => {
+  const featureImage = data.mdx.frontmatter.featureImage.childImageSharp.fixed
+
+  return (
+    <Container>
+      <FeatureImage fixed={featureImage} />
+      <Post>
+        <H1 margin="0 0 2rem 0">{data.mdx.frontmatter.title}</H1>
+        <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      </Post>
+    </Container>
+  )
+}
+
+export default singlePost
+
+export const pageQuery = graphql`
+  query SinglePostQuery($id: String!) {
+    mdx(id: { eq: $id }) {
+      body
+      frontmatter {
+        date
+        excerpt
+        slug
+        title
+        featureImage {
+          childImageSharp {
+            fixed {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    }
+  }
+`
