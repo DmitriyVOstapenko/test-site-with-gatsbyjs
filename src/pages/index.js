4d71b3593ca64f728e41fcaf7ea6bd547619033b
Dmitriy Ostapenko <dmitriy.ostapenko@outlook.com>
2020-05-09 13:39:20 +0300

    added contant card and button styled

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 //export default () => <div>Привет семье</div>
 
@@ -7,6 +7,14 @@ const Indexpage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date="March 22, 2020"
+          title="Lorem ipsum dolor si amet"
+          excerpt="Lorem ipsum bla bla bla.............."
+          slug="/test"
+        />
+      </Content>
     </Container>
   )
 }
